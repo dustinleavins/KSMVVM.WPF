9bd31908cc96ca4eee3035406d0ea485a4ba82e3
Dustin Leavins <dustinleavins@gmail.com>
2013-06-04 12:30:01 -0500

    Added some documentation to BasicMessenger class.

--- KSMVVM.WPF/Messaging/BasicMessenger.cs
+++ KSMVVM.WPF/Messaging/BasicMessenger.cs
@@ -6,13 +6,14 @@ using System.Text;
 
 namespace KSMVVM.WPF.Messaging
 {
+    /// <summary>
+    /// Basic messenging class.
+    /// </summary>
     public sealed class BasicMessenger
     {
-        static BasicMessenger()
-        {
-            defaultInstance = new Lazy<BasicMessenger>(() => new BasicMessenger());
-        }
-
+        /// <summary>
+        /// Default singleton instance of <c>BasicMessenger</c>.
+        /// </summary>
         public static BasicMessenger Default
         {
             get
@@ -21,24 +22,39 @@ namespace KSMVVM.WPF.Messaging
             }
         }
 
-        private static Lazy<BasicMessenger> defaultInstance;
+        private static Lazy<BasicMessenger> defaultInstance = 
+            new Lazy<BasicMessenger>(() => new BasicMessenger());
 
         public BasicMessenger()
         {
             responseDictionary = new ConcurrentDictionary<string, Action>();
         }
 
+        /// <summary>
+        /// Register a response <c>Action</c> for the <c>id</c>.
+        /// </summary>
+        /// <param name="id">Message id</param>
+        /// <param name="response">Message response</param>
         public void Register(string id, Action response)
         {
             responseDictionary[id] = response;
         }
 
+        /// <summary>
+        /// Unregisters the <c>Action</c> currently registered with the given id.
+        /// </summary>
+        /// <param name="id">Message id</param>
         public void Unregister(string id)
         {
             Action unused;
             responseDictionary.TryRemove(id, out unused);
         }
 
+        /// <summary>
+        /// Send a message with the given id; this will invoke the currently
+        /// registered <c>Action</c> (if one is currently registered)
+        /// </summary>
+        /// <param name="id">Message id</param>
         public void Send(string id)
         {
             Action actionHandler;
