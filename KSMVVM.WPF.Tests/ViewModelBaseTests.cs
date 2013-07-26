630aaa1377d1f6c7d8f4750ff73cc097a12e37f6
Dustin Leavins <dustinleavins@gmail.com>
2013-07-26 12:38:53 -0500

    Added ViewModelBaseTests class

---
+++ KSMVVM.WPF.Tests/ViewModelBaseTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using KSMVVM.WPF.ViewModel;
+
+namespace KSMVVM.WPF.Tests
+{
+    [TestFixture]
+    public sealed class ViewModelBaseTests
+    {
+        [Test]
+        public void OnPropertyChangedStringTest()
+        {
+            bool propertyChanged = false;
+            var target = new TargetViewModel();
+
+            target.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "StringName")
+                {
+                    propertyChanged = true;
+                }
+                
+            };
+
+            target.StringName = "Test";
+            Assert.IsTrue(propertyChanged);
+        }
+
+        [Test]
+        public void OnPropertyChangedExpressionTest()
+        {
+            bool propertyChanged = false;
+            var target = new TargetViewModel();
+
+            target.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "ExpressionName")
+                {
+                    propertyChanged = true;
+                }
+
+            };
+
+            target.ExpressionName = "Test";
+            Assert.IsTrue(propertyChanged);
+        }
+
+        private sealed class TargetViewModel : ViewModelBase
+        {
+            public string StringName
+            {
+                get
+                {
+                    return stringName;
+                }
+                set
+                {
+                    if (!string.Equals(stringName, value, StringComparison.Ordinal))
+                    {
+                        stringName = value;
+                        OnPropertyChanged("StringName");
+                    }
+                }
+            }
+
+            public string ExpressionName
+            {
+                get
+                {
+                    return expressionName;
+                }
+                set
+                {
+                    if (!string.Equals(expressionName, value, StringComparison.Ordinal))
+                    {
+                        expressionName = value;
+                        OnPropertyChanged((TargetViewModel t) => t.ExpressionName);
+                    }
+                }
+            }
+
+            private string stringName;
+            private string expressionName;
+        }
+    }
+}
