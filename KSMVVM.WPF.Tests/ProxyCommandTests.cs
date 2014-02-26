7cbb7306a64198eb7e38fb2e86d9a1d911175ec4
Dustin Leavins <dustinleavins@gmail.com>
2014-02-26 14:43:53 -0600

    Add ProxyCommand class

---
+++ KSMVVM.WPF.Tests/ProxyCommandTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2014 Dustin Leavins
+// See the file 'LICENSE.txt' for copying permission.
+using System;
+using System.Windows.Input;
+using NUnit.Framework;
+
+namespace KSMVVM.WPF.Tests
+{
+    [TestFixture]
+    public sealed class ProxyCommandTests
+    {
+        private ProxyCommand target;
+
+        [SetUp]
+        public void SetUp()
+        {
+            target = new ProxyCommand();
+        }
+
+        [Test]
+        public void CanExecuteNullBackingCommandTest()
+        {
+            Assert.IsFalse(target.CanExecute(null));
+        }
+
+        [Test]
+        public void CanExecuteTest()
+        {
+            ICommand backingCommand = new CustomCommand(
+                (x) => { },
+
+                (x) =>
+                {
+                    return x != null;
+                });
+
+            target.BackingCommand = backingCommand;
+            Assert.IsTrue(target.CanExecute("Data"));
+            Assert.IsFalse(target.CanExecute(null));
+        }
+
+        [Test]
+        public void ExecuteNullBackingCommandTest()
+        {
+            // Ensure that a NullReferenceException doesn't get thrown
+            target.Execute(null);
+        }
+        
+        [Test]
+        public void ExecuteTest()
+        {
+            bool valueToCheck = false;
+            ICommand backingCommand = new CustomCommand(
+                (x) =>
+                {
+                    valueToCheck = x != null;
+                });
+            
+            target.BackingCommand = backingCommand;
+            backingCommand.Execute("Data");
+            Assert.IsTrue(valueToCheck);
+            
+            backingCommand.Execute(null);
+            Assert.IsFalse(valueToCheck);
+        }
+    }
+}
---
+++ KSMVVM.WPF/ProxyCommand.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) 2014 Dustin Leavins
+// See the file 'LICENSE.txt' for copying permission.
+using System;
+using System.Windows.Input;
+
+namespace KSMVVM.WPF
+{
+    /// <summary>
+    /// <see cref="ICommand"/> implementation that calls methods on a 
+    /// backing <see cref="ICommand"/> instance.
+    /// </summary>
+    /// <remarks>
+    /// Use this class if you need to swap commands for a binding at runtime.
+    /// 
+    /// An example use case for a <see cref="ProxyCommand"/> instance is
+    /// to act as a command for a Window-level save command. Not every page
+    /// may be capable of saving data, and you can swap-in an appropriate
+    /// save command depending on what page you are on.
+    /// </remarks>
+    public sealed class ProxyCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            if (_cmd == null)
+            {
+                return false;
+            }
+            else
+            {
+                return _cmd.CanExecute(parameter);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            if (_cmd != null)
+            {
+                _cmd.Execute(parameter);
+            }
+        }
+
+        /// <summary>
+        /// Backing command for this instance.
+        /// </summary>
+        /// <remarks>
+        /// This property can be set to null. While it's null, this
+        /// instance's CanExecute method returns false.
+        /// </remarks>
+        public ICommand BackingCommand
+        {
+            get
+            {
+                return _cmd;
+            }
+
+            set
+            {
+                if (_cmd != null)
+                {
+                    _cmd.CanExecuteChanged -= BackingCommand_CanExecuteChanged;
+                }
+
+                if (value != null)
+                {
+                    value.CanExecuteChanged += BackingCommand_CanExecuteChanged;
+                }
+
+                _cmd = value;
+                TriggerCanExecuteChanged(new EventArgs());
+            }
+        }
+
+        private void TriggerCanExecuteChanged(EventArgs e)
+        {
+            var handler = CanExecuteChanged;
+
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        void BackingCommand_CanExecuteChanged(object sender, EventArgs e)
+        {
+            TriggerCanExecuteChanged(e);
+        }
+
+        private ICommand _cmd;
+    }
+}
