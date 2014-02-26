01a9ed338e7a6580805c9a870b10c7e46a0af1aa
Dustin Leavins <dustinleavins@gmail.com>
2014-02-26 15:11:34 -0600

    Update SolutionInfo for v0.2

--- properties/SolutionInfo.cs
+++ properties/SolutionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyFileVersion("0.2")]
 [assembly: AssemblyCompany("Dustin Leavins")]
-[assembly: AssemblyCopyright("Copyright © Dustin Leavins 2013")]
+[assembly: AssemblyCopyright("Copyright © Dustin Leavins 2013,2014")]
